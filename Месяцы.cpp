dfd2870ea72c9ab85ceca9d523b60148c0a46c87
Aleksandra <elaginas27@gmail.com>
2025-02-13 17:23:20 +0300

    4 commit

--- Месяцы.cpp
+++ Месяцы.cpp
@@ -17,57 +17,61 @@ enum Season {
 };
 int main()
 {
-   
+
     setlocale(LC_ALL, "Russian");
     int number;
     cout << "Введите номер месяца: ";
     cin >> number;
-    number = static_cast<Season>(number);
-    while (number != 0) {
-        if (number < 0 || number > 12) { 
-            cout << "Неправильный номер!\n"; }
-        switch (number) {
-        case  Январь:
-            cout << "Январь\n";
-            break;
-        case  Февраль:
-            cout << "Февраль\n";
-            break;
-        case  Март:
-            cout << "Март\n";
-            break;
-        case  Апрель:
-            cout << "Апрель\n";
-            break;
-        case  Май:
-            cout << "Май\n";
-            break;
-        case  Июнь:
-            cout << "Июнь\n";
-            break;
-        case  Июль:
-            cout << "Июль\n";
-            break;
-        case  Август:
-            cout << "Август\n";
-            break;
-        case  Сентябрь:
-            cout << "Сентябрь\n";
-            break;
-        case  Октябрь:
-            cout << "Октябрь\n";
-            break;
-        case  Ноябрь:
-            cout << "Ноябрь\n";
-            break;
-        case  Декабрь:
-            cout << "Декабрь\n";
-            break;
-            
+    Season snumber = static_cast<Season>(number);
+    while (snumber != 0) {
+        if (snumber < 0 || snumber > 12) {
+            cout << "Неправильный номер!\n";
+
+        }
+        else {
+            switch (snumber) {
+            case  Январь:
+                cout << "Январь\n";
+                break;
+            case  Февраль:
+                cout << "Февраль\n";
+                break;
+            case  Март:
+                cout << "Март\n";
+                break;
+            case  Апрель:
+                cout << "Апрель\n";
+                break;
+            case  Май:
+                cout << "Май\n";
+                break;
+            case  Июнь:
+                cout << "Июнь\n";
+                break;
+            case  Июль:
+                cout << "Июль\n";
+                break;
+            case  Август:
+                cout << "Август\n";
+                break;
+            case  Сентябрь:
+                cout << "Сентябрь\n";
+                break;
+            case  Октябрь:
+                cout << "Октябрь\n";
+                break;
+            case  Ноябрь:
+                cout << "Ноябрь\n";
+                break;
+            case  Декабрь:
+                cout << "Декабрь\n";
+                break;
+
+            }
         }
         cout << "Введите номер месяца: ";
         cin >> number;
-        number = static_cast<Season>(number);
+        snumber = static_cast<Season>(number);
     }
     cout << "До свидания.";
 }
